6523f67425d2f444a4ea832ed69411242dabe49b
h5law <dev@h5law.com>
2026-01-31 08:25:35 +0000

    feat: add extra detail to rss button and feed

--- templates/k3vl4r++/script.js
+++ templates/k3vl4r++/script.js
@@ -1,10 +1,9 @@
 const themeBtn = document.getElementById('themeBtn')
 const body = document.getElementsByTagName('body')[0]
 
-window.matchMedia('prefers-color-scheme: dark').matches ? 
+window.matchMedia('prefers-color-scheme: dark').matches ?
   body.classList.add('light') :
   body.classList.add('dark');
-
 
 themeBtn.innerText = body.classList.contains('dark') ? "Light" : "Dark";
 
@@ -20,4 +19,3 @@ themeBtn.addEventListener("click", () => {
   themeBtn.innerText = body.classList.contains('dark') ? "Light" : "Dark";
 
 })
-
